5a2793abc7aae7973a6075de352be5445e398be9
ZixiongLin <32196374+ZixiongLin@users.noreply.github.com>
2024-07-22 21:20:27 +0100

    Added Sharp dependencie

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,3 @@
-import { defineConfig, passthroughImageService } from 'astro/config';
+import { defineConfig } from 'astro/config';
 // https://astro.build/config
-export default defineConfig({
-  image:{
-    service:passthroughImageService()
-  }
-});
+export default defineConfig({});
